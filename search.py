800c601f36548f1b035f27aa203f8ec13cc04bf8
nakamura <nakamu28_uver@icloud.com>
2021-02-28 10:12:39 +0900

    first commit

--- search.py
+++ search.py
@@ -1,9 +1,18 @@
+import os
+import csv
 
-### 検索ツールサンプル
-### これをベースに課題の内容を追記してください
+#csvファイル読み込み
+path = './lesson01_test2.csv'
+source = []
+if os.path.isfile(path) == True:
+    with open(path) as f:
+        reader = csv.reader(f)
+        source = [row for row in reader]
+
+print(source)
 
 # 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
+#source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 ### 検索ツール
 def search():
@@ -11,7 +20,25 @@ def search():
     
     ### ここに検索ロジックを書く
 
-    print("{}が見つかりした".format(word))
+    #listにある場合、リストに存在する出力をして、終了
+    for row in source:
+        for sourceWord in row:
+            if sourceWord == word:
+                print("{}が見つかりました".format(word))
+                return 0
+
+    #リストにない場合、リストにないことを出力
+    print("リストにはありませんでした")
+    #リストに追加（今回は一行のみなので、source[0]に追加）
+    source[0].append(word)
+    
+    #csvファイルに書き込み
+    with open(path, 'w') as f:
+        writer = csv.writer(f)
+        for row in source:
+            writer.writerow(row)
+
+    print("{}を追加しました".format(word))
 
 if __name__ == "__main__":
     search()
